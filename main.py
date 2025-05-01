7ae30b6d35fd12e1d99cf991ae76b3261fd1f0c6
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-05-01 09:52:34 -0700

    stuck

--- main.py
+++ main.py
@@ -3,14 +3,47 @@
     go off of UTC
     add hours depending on time zone chosen
     determine if time zone uses daylight savings and adjust from that point
+    slider y/n for if you want to adjust for daylight savings
 
 """
 
 import tkinter as tk
 import time
 from datetime import datetime
+from pathlib import Path
+import json
 
-def update_time():
+timezone_offsets = {
+    "UTC": 0,        # Coordinated Universal Time
+    "CET": 1,        # Central European Time
+    "EET": 2,        # Eastern European Time
+    "MSK": 3,        # Moscow Standard Time
+    "GST": 4,        # Gulf Standard Time
+    "IST": 5.5,      # India Standard Time
+    "BST (BD)": 6,   # Bangladesh Standard Time
+    "ICT": 7,        # Indochina Time
+    "CST (China)": 8,# China Standard Time
+    "JST": 9,        # Japan Standard Time
+    "AEST": 10,      # Australian Eastern Standard Time
+    "NZST": 12,      # New Zealand Standard Time
+    "NST": -3.5,     # Newfoundland Standard Time
+    "ART": -3,       # Argentina Time
+    "AST": -4,       # Atlantic Standard Time
+    "EST": -5,       # Eastern Standard Time
+    "CST": -6,       # Central Standard Time
+    "MST": -7,       # Mountain Standard Time
+    "PST": -8,       # Pacific Standard Time
+    "AKST": -9,      # Alaska Standard Time
+    "HST": -10       # Hawaii Standard Time
+}
+
+path = Path("timezone_offsets.json")
+contents = json.dumps(timezone_offsets)
+path.write_text(contents)
+
+print(contents)
+
+def update_time(timezone = 0):
     current_UTC = datetime.now()
     print(f"local time: {datetime.ctime(current_UTC)}")
 
